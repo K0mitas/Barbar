aed94fcd7970cd7ff37c9c3dc85e10b3e3f9f5cb
komitas <simonyankaren.16@yandex.ru>
2024-11-10 12:04:26 +0400

    Reading name button

---
+++ src/Pages/Home.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+
+export default function Home() {
+  return (
+    <div>
+
+          <h1 className='home-h1'>BarBar</h1>
+
+            <div className='home-block-button'>
+
+              <p className='home-block-text'>Приветствую, дорогой пользователь этого веб-приложения! 
+                Рад видеть тебя здесь. Цель этой игры — проверить твой словарный запас армянского языка. 
+                Суть игры в том, чтобы угадать, как что-то будет называться на армянском языке. 
+                Желаю удачной игры!</p>
+
+                <NavLink to="/language">
+                   <button className='home-button'>Вместе</button>
+                </NavLink>
+
+            </div>
+
+    </div>
+  )
+}
